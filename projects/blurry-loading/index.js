64d18e8942b9c300ad4f65ffa23f1dd4c4f2dde8
Nekrasov-A-A <a.alex.nekrasov@gmail.com>
2021-03-19 21:52:59 +0300

    added mobile bgImage for 5 project

--- projects/blurry-loading/index.js
+++ projects/blurry-loading/index.js
@@ -1,19 +1,19 @@
-const loader = document.getElementById("loader");
-const bgImg = document.getElementById("bg-img");
+const loaderEl = document.getElementById("loader");
+const bgImageEl = document.getElementById("bg-img");
 let startValue = 0;
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
+const setValue = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
-const fn = () => {
+const changeBlur = () => {
   startValue++;
   if (startValue > 99) {
-    clearInterval(interval);
+    clearInterval(intervalId);
   }
-  loader.innerHTML = `${startValue}%`;
-  loader.style.opacity = scale(startValue, 0, 100, 1, 0);
-  bgImg.style.filter = `blur(${scale(startValue, 0, 100, 100, 0)}px)`;
+  loaderEl.innerHTML = `${startValue}%`;
+  loaderEl.style.opacity = setValue(startValue, 0, 100, 1, 0);
+  bgImageEl.style.filter = `blur(${setValue(startValue, 0, 100, 100, 0)}px)`;
 };
 
-const interval = setInterval(fn, 30);
+const intervalId = setInterval(changeBlur, 30);
