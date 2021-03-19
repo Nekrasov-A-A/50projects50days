2c5a3dfb8a64c7f452d525a79ad61fadf801f377
Nekrasov-A-A <a.alex.nekrasov@gmail.com>
2021-03-19 17:36:37 +0300

    added mobile version for 40 project

--- projects/3d-background-boxes/index.js
+++ projects/3d-background-boxes/index.js
@@ -1,16 +1,24 @@
 const btn = document.getElementById("btn");
-const container = document.getElementById("container");
+const containerEl = document.getElementById("container");
+const containerSide = window.getComputedStyle(container).width;
+const containerSideNumber = +containerSide.substring(
+  0,
+  containerSide.length - 2
+);
+const shiftBGPosition = containerSideNumber / 4;
 
 btn.addEventListener("click", () => container.classList.toggle("big"));
 
-const createBox = () => {
+const createBoxes = () => {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
-      container.appendChild(box);
+      box.style.backgroundPosition = `${-j * shiftBGPosition}px ${
+        -i * shiftBGPosition
+      }px`;
+      containerEl.appendChild(box);
     }
   }
 };
-createBox();
+createBoxes();
